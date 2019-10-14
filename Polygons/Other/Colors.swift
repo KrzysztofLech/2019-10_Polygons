1797022fbc5336383ee02014d87f9ae056040ce4
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-10-14 17:39:45 +0200

    Colors paette added

---
+++ Polygons/Other/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Polygons
+//
+//  Created by Krzysztof Lech on 14/10/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+enum Colors {
+    
+    static let color0 = UIColor(named: "Color1-0")!
+    
+    static let color1 = UIColor(named: "Color1-1")!
+    static let color2 = UIColor(named: "Color1-2")!
+    static let color3 = UIColor(named: "Color1-3")!
+    static let color4 = UIColor(named: "Color1-4")!
+    static let color5 = UIColor(named: "Color1-5")!
+    
+    static let color9 = UIColor(named: "Color1-9")!
+}
