a3bef266498d3e8575507eb69d219f20a4ab58b0
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-10-14 16:31:21 +0200

    Initial commit

---
+++ Polygons/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Polygons
+//
+//  Created by Krzysztof Lech on 14/10/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
